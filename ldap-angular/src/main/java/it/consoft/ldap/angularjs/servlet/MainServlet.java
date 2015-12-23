d9735cec7cb48567c0c4f94e847eeadd006672ec
mcorallo <mcorallo74@gmail.com>
2015-12-23 17:28:25 +0100

    rest proxy servlet

--- ldap-angular/src/main/java/it/consoft/ldap/angularjs/servlet/MainServlet.java
+++ ldap-angular/src/main/java/it/consoft/ldap/angularjs/servlet/MainServlet.java
@@ -7,34 +7,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebServlet("/main")
 @SuppressWarnings("serial")
 public class MainServlet extends HttpServlet {
+	private static final Logger logger = LoggerFactory.getLogger(MainServlet.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		String cmd = req.getParameter("command");
-//		switch (cmd) {
-//		case "loadPhones": {
-//			InputStream stream = this.getClass().getResourceAsStream("/phones.json");
-//			try (PrintWriter writer = resp.getWriter()) {
-//				writer.write(IOUtils.toString(stream));
-//			}
-//			break;
-//		}
-//		case "phoneDetail": {
-//			String phoneId = req.getParameter("phoneId");
-//			InputStream stream = this.getClass().getResourceAsStream("/phones/" + phoneId);
-//			try (PrintWriter writer = resp.getWriter()) {
-//				writer.write(IOUtils.toString(stream));
-//			}
-//			break;
-//		}
-//		default:
-//			resp.sendError(HttpServletResponse.SC_NOT_FOUND, "command " + cmd + " not found");
-//			break;
-//		}
+		String restRequest = req.getParameter("request");
+		if (restRequest == null || restRequest.isEmpty()) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
+		Client client = ClientBuilder.newClient().register(JacksonFeature.class);
+		WebTarget target = client.target("http://localhost:8080/ldap-rest/rest" + restRequest);
+		String restResult = target.request().get(String.class);
+		logger.debug("received rest response: " + restResult);
+		resp.getWriter().write(restResult);
 	}
 
 }
