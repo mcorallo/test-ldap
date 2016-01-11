ac79a4d3d02839e154b8af17f213ec50d7d855a6
mcorallo <mcorallo74@gmail.com>
2016-01-11 16:28:56 +0100

    locale settings

---
+++ ldap-web-spring/src/main/java/it/consoft/ldap/web/filter/LocalizationFilter.java
@@ -0,0 +1,58 @@
+package it.consoft.ldap.web.filter;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import it.consoft.ldap.web.utils.LocalizationManager;
+
+public class LocalizationFilter implements Filter {
+
+	public static final String SESSION_LANG = "session_lang";
+
+	private static LocalizationManager localizationManager;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+
+		req.setAttribute("labels", localizationManager);
+		String lang = req.getParameter("lang");
+		if (lang == null) {
+			lang = (String) req.getSession().getAttribute(SESSION_LANG);
+		}
+		if (lang != null && !lang.isEmpty()) {
+			Locale locale = new Locale(lang);
+			localizationManager.setLocale(locale);
+		}
+		chain.doFilter(request, response);
+	}
+
+	public static LocalizationManager getLocalizationManager() {
+		return localizationManager;
+	}
+
+	public static void setLocalizationManager(LocalizationManager localizationManager) {
+		LocalizationFilter.localizationManager = localizationManager;
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ldap-web-spring/src/main/webapp/WEB-INF/views/jsp/login.jsp
+++ ldap-web-spring/src/main/webapp/WEB-INF/views/jsp/login.jsp
@@ -1,6 +1,9 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,25 +24,23 @@
 <body>
 
 	<div class="container">
-
-
 		<h2 class="form-signin-heading text-center">Please sign in</h2>
 
 		<form class="form-horizontal col-sm-6 col-sm-offset-3 well" method="post">
 			<div class="form-group">
-				<label for="username" class="col-sm-2 control-label">Username</label>
-				<div class="col-sm-10">
+				<label for="username" class="col-sm-4 control-label">${labels.get('username')}</label>
+				<div class="col-sm-8">
 					<input class="form-control" name="username" placeholder="Username">
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="password" class="col-sm-2 control-label">Password</label>
-				<div class="col-sm-10">
+				<label for="password" class="col-sm-4 control-label">Password</label>
+				<div class="col-sm-8">
 					<input type="password" class="form-control" name="password" placeholder="Password">
 				</div>
 			</div>
 			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
+				<div class="col-sm-offset-4 col-sm-8">
 					<button type="submit" class="btn btn-primary">Sign in</button>
 				</div>
 			</div>
