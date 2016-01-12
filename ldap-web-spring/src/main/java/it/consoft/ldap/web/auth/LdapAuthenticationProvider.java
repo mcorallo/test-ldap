d130a4e8660161d1094939edc3c8929ae47c7fb2
mcorallo <mcorallo74@gmail.com>
2016-01-12 17:42:37 +0100

    shared utils refactoring

--- ldap-web-spring/src/main/java/it/consoft/ldap/web/auth/LdapAuthenticationProvider.java
+++ ldap-web-spring/src/main/java/it/consoft/ldap/web/auth/LdapAuthenticationProvider.java
@@ -1,7 +1,6 @@
 package it.consoft.ldap.web.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,14 +13,14 @@ import it.consoft.ldap.web.manager.AuthManager;
 public class LdapAuthenticationProvider implements AuthenticationProvider {
 
 	@Autowired
-	private ApplicationContext applicationContext;
+	private AuthManager authManager;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String name = authentication.getName();
 		String password = authentication.getCredentials().toString();
 
-		Authentication auth = applicationContext.getBean(AuthManager.class).authenticate(name, password);
+		Authentication auth = authManager.authenticate(name, password);
 		return auth;
 	}
 
