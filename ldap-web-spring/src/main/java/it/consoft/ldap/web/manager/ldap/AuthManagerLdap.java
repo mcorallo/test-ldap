f01dc4fbaf744f6bda4ed458c07628a45cdb076f
mcorallo <mcorallo74@gmail.com>
2016-01-13 17:52:01 +0100

    added technical admin user

--- ldap-web-spring/src/main/java/it/consoft/ldap/web/manager/ldap/AuthManagerLdap.java
+++ ldap-web-spring/src/main/java/it/consoft/ldap/web/manager/ldap/AuthManagerLdap.java
@@ -7,10 +7,6 @@ import java.util.Map;
 import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import it.consoft.ldap.example.rest.bean.User;
 import it.consoft.ldap.web.manager.AuthManager;
@@ -20,13 +16,13 @@ import it.consoft.shared.ldap.Ldap;
 import it.consoft.shared.ldap.LdapConfiguration;
 
 //@Service
-public class AuthManagerLdap implements AuthManager {
+public class AuthManagerLdap extends AuthManager {
 	private static final Logger logger = LoggerFactory.getLogger(AuthManagerLdap.class);
 
 	private static List<Ldap> ldapServers = new ArrayList<>();
 
 	static {
-		ConfigurationManager cm = WebUtils.getConfigurationmanager();
+		ConfigurationManager cm = WebUtils.getConfigurationManager();
 		String[] ldapProviders = cm.getStringArray("ldap.providers");
 		for (String provider : ldapProviders) {
 			Map<String, String> subset = cm.getSubset("ldap." + provider);
@@ -36,27 +32,7 @@ public class AuthManagerLdap implements AuthManager {
 	}
 
 	@Override
-	public Authentication authenticate(String username, String password) {
-
-		User user = getUser(username, password);
-		if (user == null) {
-			return null;
-		}
-
-		List<GrantedAuthority> grantedAuths = new ArrayList<>();
-		for (String s : user.getGroups()) {
-			grantedAuths.add(new SimpleGrantedAuthority(getGrantedAuth(s)));
-		}
-		Authentication auth = new UsernamePasswordAuthenticationToken(user.getUsername(), password, grantedAuths);
-		return auth;
-	}
-
-	private static String getGrantedAuth(String memberOf) {
-		return "ROLE_" + memberOf.toUpperCase();
-	}
-
-	User getUser(String username, String password) {
-
+	public User loadUser(String username, String password) {
 		try {
 			logger.info("login request for user {}", username);
 			it.consoft.shared.ldap.User user = null;
