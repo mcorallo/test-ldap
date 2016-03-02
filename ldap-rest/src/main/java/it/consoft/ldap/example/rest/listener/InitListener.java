725508b532d8da0bbaa77a0f00e42656c73270a2
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-03-02 21:35:37 +0100

    Added missing table's create

--- ldap-rest/src/main/java/it/consoft/ldap/example/rest/listener/InitListener.java
+++ ldap-rest/src/main/java/it/consoft/ldap/example/rest/listener/InitListener.java
@@ -1,5 +1,4 @@
 package it.consoft.ldap.example.rest.listener;
-
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
@@ -42,6 +41,13 @@ public class InitListener implements ServletContextListener {
 			}
 
 			ConfigurationManager queries = RestUtils.getQueries();
+			
+			//creates all necessary tables
+			int executeUpdate = queryHelper.executeUpdate("CREATE TABLE IF NOT EXISTS users (id bigint auto_increment, username varchar(50), email varchar(50))");
+			executeUpdate = queryHelper.executeUpdate("CREATE TABLE IF NOT EXISTS user_groups (user_id bigint, local_group_name varchar(50))");
+			executeUpdate = queryHelper.executeUpdate("CREATE TABLE IF NOT EXISTS ldap_local_group_rel (ldap_group_name varchar(50), local_group_name varchar(50))");
+			
+			
 			List<Record> rs = queryHelper.select(queries.getProperty("ldap_local_group_rel.count.1"));
 			Long count = rs.get(0).getValue("TOT");
 			if (count == 0) {
